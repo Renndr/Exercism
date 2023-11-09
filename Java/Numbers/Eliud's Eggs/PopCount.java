3c44b65771034ab96e4581d53f8ac8501d36149a
Renndr <manueldejbernabel@gmail.com>
2023-11-09 11:58:47 -0400

    update numbers solution

--- Java/Numbers/Eliud's Eggs/PopCount.java
+++ Java/Numbers/Eliud's Eggs/PopCount.java
@@ -1,12 +1,19 @@
 public class PopCount {
     public int eggCount(int number) {
-        return Integer.bitCount(number);
+        int count = 0;
+        while(number > 0) {
+            count += number % 2 == 1 ? 1 : 0;
+            number /= 2;
+            System.out.println(number);
+        }
+        return count;
     }
 }
 
 class Main {
     public static void main(String[] args) {
         PopCount popCount = new PopCount();
-        System.out.println(popCount.eggCount(999999999));
+        System.out.println(popCount.eggCount(156));
     }
 }
+
