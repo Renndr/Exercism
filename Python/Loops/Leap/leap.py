5ebf4cedd3ea82c1d322a6febcd31f22ff20cfbf
Renndr <manueldejbernabel@gmail.com>
2024-01-05 19:58:06 -0400

    add leap exercise

---
+++ Python/Loops/Leap/leap.py
@@ -0,0 +1,10 @@
+#This is a function that takes a year as a parameter and returns True if the year is a leap year, False otherwise.
+#The function should return True if the year is evenly divisible by 4
+#except every year that is evenly divisible by 100
+#unless the year is also evenly divisible by 400
+#For example, 1997 is not a leap year, but 1996 is.  1900 is not a leap year, but 2000 is.
+
+def leap_year(year):
+    if year % 4 == 0 & year % 100 != 0 & year % 400 == 0:
+        return True
+    
